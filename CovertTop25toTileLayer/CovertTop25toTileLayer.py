4caf20723818cc30ff8cdea87666d9c9eb1b4c7c
pimverver <pim@travelingo.nl>
2015-07-27 17:26:38 +0200

    yes

--- CovertTop25toTileLayer/CovertTop25toTileLayer.py
+++ CovertTop25toTileLayer/CovertTop25toTileLayer.py
@@ -1,1 +1,14 @@
-import
+﻿import sys
+
+try:
+    from osgeo import gdal
+    from osgeo import osr
+except:
+    import gdal
+    print('You are using "old gen" bindings. gdal2tiles needs "new gen" bindings.')
+    sys.exit(1)
+
+import os
+import math
+
+os.system(gdal2tiles.py -h)
